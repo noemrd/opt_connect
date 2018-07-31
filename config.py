74c6f114044547157eadadb6b0ddd9a8b4e60292
noemrd <muriel@bytefoods.co>
2018-07-31 13:41:28 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+OPT_CONNECT_CONFIG = {'accountId': 123,
+            'applicationId': 123,
+            'secret': 'xxxxxxxx'}
+
+PSQL_STRING = 'username:password@hostname:port/database'
+
+OPTIMOROUTE_KEY = 'xxxxxxxx'
